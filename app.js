db82cc6cdbd03dd64d60127331a4ba87777f48cf
DylanQuach <find@Glenda.localdomain>
2018-11-28 17:18:02 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,8 @@
+//Countdown to December 25th
+
+//The Date for Today
+//the date we want
+//calculation to know how much time is left
+//Display on HTML
+
+//Eventlistener, onchange, the change of the difference calculation
